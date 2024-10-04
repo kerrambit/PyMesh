581cacace7f22c6858c77aa773b4ac3277d92bdf
Marek Eibel <536376@mail.muni.cz>
2024-10-04 09:56:43 +0200

    feat: extend convertor with InputMeshFile
    the reason is that every file type can take also different arguments when data are being loaded

--- src/convertor/convertor.py
+++ src/convertor/convertor.py
@@ -1,33 +1,76 @@
-import pymeshlab
+from pymeshlab import MeshSet
 from enum import Enum
 from pathlib import Path
 from abc import ABC
+from typing import Tuple
 
 
 class FileFormat(Enum):
     STL = 0
     OBJ = 1
-    PLY = 2
 
     def __str__(self):
         if self == FileFormat.STL:
             return "stl"
         elif self == FileFormat.OBJ:
             return "obj"
-        elif self == FileFormat.PLY:
-            return "ply"
+        else:
+            return "unkown"
+
+
+class InputMeshFile(ABC):
+
+    def __init__(self, file_format: FileFormat, mesh_file_path: Path) -> None:
+        self.__file_format = file_format
+        self.mesh_file_path = mesh_file_path
+
+    def get_type(self) -> FileFormat:
+        return self.__file_format
+
+    def get_full_import_filepath(self) -> str:
+        return str(self.mesh_file_path)
+
+
+class STLInputMeshFile(InputMeshFile):
+    def __init__(self, mesh_file_path: Path, unify_vertices: True) -> None:
+        super().__init__(FileFormat.STL, mesh_file_path)
+        self.unify_vertices = unify_vertices
+
+
+class OBJInputMeshFile(InputMeshFile):
+    def __init__(self, mesh_file_path: Path) -> None:
+        super().__init__(FileFormat.OBJ, mesh_file_path)
 
 
 class OutputMeshFile(ABC):
 
     def __init__(
-        self, file_format: FileFormat, directory_path: Path, filename: str
+        self,
+        file_format: FileFormat,
+        directory_path: Path,
+        filename: str,
+        save_textures: bool,
+        texture_quality: int,
     ) -> None:
         self.__type: FileFormat = file_format
         self.path: Path = directory_path
         self.filename: str = filename
+        self.save_textures: bool = save_textures
+        self.texture_quality: int = texture_quality
 
-    def get_export_file_path(self) -> str:
+    def __init__(
+        self,
+        file_format: FileFormat,
+        directory_path: Path,
+        filename: str,
+    ) -> None:
+        self.__type: FileFormat = file_format
+        self.path: Path = directory_path
+        self.filename: str = filename
+        self.save_textures: bool = True
+        self.texture_quality: int = -1
+
+    def get_full_export_filepath(self) -> str:
         return str(self.path / f"{self.filename}.{self.get_type()}")
 
     def get_type(self) -> FileFormat:
@@ -35,6 +78,21 @@ class OutputMeshFile(ABC):
 
 
 class STLOutputMeshFile(OutputMeshFile):
+    def __init__(
+        self,
+        path: Path,
+        filename: str,
+        save_textures: bool,
+        texture_quality: int,
+        binary: bool = False,
+        colormode: bool = False,
+        save_face_color: bool = False,
+    ) -> None:
+        super().__init__(FileFormat.STL, path, filename, save_textures, texture_quality)
+        self.binary: bool = binary
+        self.colormode: bool = colormode
+        self.save_face_color: bool = save_face_color
+
     def __init__(
         self,
         path: Path,
@@ -50,15 +108,40 @@ class STLOutputMeshFile(OutputMeshFile):
 
 
 class OBJOutputMeshFile(OutputMeshFile):
+    def __init__(
+        self, path: Path, filename: str, save_textures: bool, texture_quality: int
+    ) -> None:
+        super().__init__(FileFormat.OBJ, path, filename, save_textures, texture_quality)
+
     def __init__(self, path: Path, filename: str) -> None:
         super().__init__(FileFormat.OBJ, path, filename)
 
 
-def convert_mesh(input_file_path: Path, output_file: OutputMeshFile):
+def convert_mesh(input_file: InputMeshFile, output_file: OutputMeshFile):
 
-    mesh_set = pymeshlab.MeshSet()
-    mesh_set.load_new_mesh(str(input_file_path))
+    mesh_set = MeshSet()
+    full_input_path = input_file.get_full_import_filepath()
 
-    full_path = output_file.get_export_file_path()
-    mesh_set.save_current_mesh(full_path)
+    if input_file.get_type() == FileFormat.OBJ:
+        mesh_set.load_new_mesh(full_input_path)
+    else:
+        mesh_set.load_new_mesh(full_input_path)
+
+    full_output_path = output_file.get_full_export_filepath()
+    basic_properties: Tuple[str, bool, int] = (
+        full_output_path,
+        output_file.save_textures,
+        output_file.texture_quality,
+    )
+
+    if output_file.get_type() == FileFormat.STL:
+        mesh_set.save_current_mesh(
+            *basic_properties,
+            binary=output_file.binary,
+            colormode=output_file.colormode,
+            save_face_color=output_file.save_face_color,
+        )
+    else:
+        mesh_set.save_current_mesh(*basic_properties)
+
 
